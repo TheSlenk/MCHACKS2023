2d1512adb28bcefde6063bb90e2bf53a8b9bb22e
SlenK <stephenmamosian@gmail.com>
2024-01-27 13:56:37 -0500

    Basic Server With User Location Code

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('script'));
+app.set('view engine', 'pug');
+app.set('views', 'view');
+
 app.get('/', (req, res) => {
-    
+    res.redirect('/home');
 });
+
+app.get('/home', (req, res) => {
+    res.status(200).render('home');
+});
+
 app.listen(3000);
+console.log('Server running on port 3000...');
