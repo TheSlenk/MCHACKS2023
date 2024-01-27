e1911ac3f47d0761c913a78ce20929ee3df6518f
Kayeon Han <kayeonalvina@gmail.com>
2024-01-27 16:43:08 -0500

    Retrieving closeby info
    I think its working but it might not work.

--- script/home.js
+++ script/home.js
@@ -1,18 +1,55 @@
 const refreshFrequency = 1000; // 1 second
 const demoTextBox = document.getElementById("demo");
+let longitude;
+let latitude;
+const radius = 100;
+
 function getLocation() {
+
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(showPosition);
     } else {
         alert("Geolocation is not supported by this browser.");
     }
     console.log("ran");
+    
+    nearby((error, results) => {
+        if (error) {
+          console.error(error);
+        } else {
+          // Process the results here
+          console.log(results);
+        }
+    });
+
+
     setTimeout(getLocation, refreshFrequency);
 }
 function showPosition(position) {
-    demoTextBox.innerHTML = "Latitude: " + position.coords.latitude +"<br>Longitude: " + position.coords.longitude;
+    longitude = position.coords.longitude;
+    latitude = position.coords.latitude;
+
+    demoTextBox.innerHTML = "Latitude: " + latitude +"<br>Longitude: " + longitude;
 }
 
 function init() {
     getLocation();
+}
+
+function nearby(callback) {
+    const placesService = new google.maps.places.PlacesService(document.createElement('div'));
+    let location = new google.maps.LatLng(latitude, longitude);
+    
+    const request = {
+      location: location,
+      radius: radius
+    };
+
+    placesService.nearbySearch(request, (results, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        callback(null, results);
+      } else {
+        callback(`Nearby search failed: ${status}`, null);
+      }
+    });
 }
