c14a9fd064962390a5e23ace9935780cd7f83c4c
ryushen-tan <78460980+ryushen-tan@users.noreply.github.com>
2024-01-27 14:27:51 -0500

    Fixed Routing and made camera work :D

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 app.set("view engine", "pug");
-app.set("views", "./views");
-app.use(express.json());
-app.use(express.static('views'));
+app.set("views", path.join(__dirname, "./views"));
+
+// Serve static files from the 'views/pages' directory
+app.use(express.static(path.join(__dirname, 'views/pages')));
 
 app.get('/', async function (req, res) {
     try {
-      res.render('pages/demo');
-    } catch (error){
-      console.error('Error getting demo page: ', error);
-      res.status(500).send('Internal Server Error');
+        res.render('pages/demo');
+    } catch (error) {
+        console.error('Error getting demo page: ', error);
+        res.status(500).send('Internal Server Error');
     }
-  });
-app.listen(3000);
-console.log(`Connected to http://localhost:3000`);
+});
+
+app.listen(3000, () => {
+    console.log(`Connected to http://localhost:3000`);
+});
