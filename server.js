81aba1ca6225ab3b82c3a79a1b2b26db06c8bc71
ryushen-tan <78460980+ryushen-tan@users.noreply.github.com>
2024-01-27 14:07:39 -0500

    Webcam

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+app.set("view engine", "pug");
+app.set("views", "./views");
+app.use(express.json());
+app.use(express.static('views'));
 
-app.get('/', (req, res) => {
-    
-});
+app.get('/', async function (req, res) {
+    try {
+      res.render('pages/demo');
+    } catch (error){
+      console.error('Error getting demo page: ', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
 app.listen(3000);
+console.log(`Connected to http://localhost:3000`);
