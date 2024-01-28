e37feca7e7e02a13a7b56ea8f24d08ce3983a608
ryushen-tan <78460980+ryushen-tan@users.noreply.github.com>
2024-01-27 19:40:44 -0500

    Made camera button Toggle

--- views/pages/js/client.js
+++ views/pages/js/client.js
@@ -2,22 +2,26 @@ document.addEventListener("DOMContentLoaded", () => {
     let but = document.getElementById("but");
     let video = document.getElementById("vid");
     let mediaDevices = navigator.mediaDevices;
+    let videoStream = null; // Variable to store the video stream
+
     vid.muted = true;
+
     but.addEventListener("click", () => {
-        // Accessing the user camera and video.
-        mediaDevices
-            .getUserMedia({
-                video: true,
-                audio: true,
-            })
-            .then((stream) => {
-                // Changing the source of video to current stream.
-                video.srcObject = stream;
-                video.addEventListener("loadedmetadata", () => {
-                    video.play();
-                });
-            })
-            .catch(alert);
+        if (videoStream) {
+            videoStream.getTracks().forEach(track => track.stop());
+            video.srcObject = null;
+            videoStream = null;
+        } else {
+            mediaDevices
+                .getUserMedia({ video: true })
+                .then((stream) => {
+                    videoStream = stream;
+                    video.srcObject = stream;
+                    video.addEventListener("loadedmetadata", () => {
+                        video.play();
+                    });
+                })
+                .catch(alert);
+        }
     });
 });
-
